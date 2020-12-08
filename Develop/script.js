0d399ce96776409221c1d8b9c6d345e23e804126
Nick Vandenberge <nvandenberge14@gmail.com>
2020-12-08 15:58:18 -0500

    added function to create arrays of numbers that represents character codes and  validation for user to select at least one character type

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,33 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Creates an array of numbers that represents Uppercase character codes
+const uppercase_char_codes = arrayFromLowToHigh(65, 90);
 
-  passwordText.value = password;
-}
+// Creates an array of numbers that represents Lowercase character codes
+const lowercase_char_codes = arrayFromLowToHigh(97, 122);
+
+// Creates an array of numbers that represents Number character codes
+const number_char_codes = arrayFromLowToHigh(48, 57);
+
+// Creates an array of numbers that represents Special character codes
+const special_char_codes = arrayFromLowToHigh(33, 47)
+  .concat(arrayFromLowToHigh(58, 64))
+  .concat(arrayFromLowToHigh(91, 96))
+  .concat(arrayFromLowToHigh(123, 126));
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", getPasswordOptions);
 
+// Function that takes in a low and high number and creates an array that includes all numbers between them
+// This elimninates the need to create an array with each inidividual character ex: ['a', 'b', 'c']
+function arrayFromLowToHigh(low, high) {
+  const array = [];
+  for (let i = low; i <= high; i++) {
+    array.push(i);
+  }
+  return array;
+}
 
 // Prompts to get password length and options when user clicks 'Generate Password'
 function getPasswordOptions() {
@@ -19,27 +35,41 @@ function getPasswordOptions() {
     "How many characters would you like your password to be?"
   );
 
+  // Validates that password length is between 8-128 and input was a number, else start over.
   if (passwordLength < 8 || passwordLength >= 129 || isNaN(passwordLength)) {
-    alert(
-      "Your password length must be a number 8 - 128."
-    );
+    alert("Your password length must be a number between 8 - 128.");
     getPasswordOptions();
-    return
+    return;
   }
 
-  const includeLowercase = confirm(
-    "Would you like your password to include lowercase characters?"
-  );
+  // 4 prompts asking the user which character sets to include in password
+  const includeLowercase = confirm("Click OK to include lowercase characters?");
 
-  const includeUppercase = confirm(
-    "Would you like your password to include uppercase characters?"
-  );
+  const includeUppercase = confirm("Click OK to include uppercase characters?");
 
-  const includeNumbers = confirm(
-    "Would you like to your password to include numbers?"
-  );
+  const includeNumbers = confirm("Click OK to include numbers?");
 
-  const includeSpecial = confirm(
-    "Would you like your password to include special characters?"
-  );
+  const includeSpecial = confirm("Click OK to include special characters?");
+
+  // Validates that at least one set of characters was selected, else start over
+  if (
+    !includeLowercase &&
+    !includeUppercase &&
+    !includeNumbers &&
+    !includeSpecial
+  ) {
+    alert(
+      "Please select at least one character type to include in your password."
+    );
+    getPasswordOptions();
+    return;
+  }
+}
+
+// Write password to the #password input
+function writePassword(passwordResult) {
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
+
+  passwordText.value = passwordResult;
 }
